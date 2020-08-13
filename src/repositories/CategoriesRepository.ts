6ea0b3b1d167279e7527e3fd3e813c289418a6ec
Paulo Albuquerque <pcajunior@gmail.com>
2020-08-13 20:18:26 -0300

    Ajuste de chamadas assíncronas

--- src/repositories/CategoriesRepository.ts
+++ src/repositories/CategoriesRepository.ts
@@ -1,34 +1,25 @@
 import BASE_URL from '../config';
 import { CategoriasInterface } from '../types/video';
 
-export const getCategoriesFull = (): Promise<[]> => {
-  const getData = async () => {
-    const response = await fetch(`${BASE_URL}/categorias?_embed=videos`);
-    return response.json();
-  };
-  return getData();
+export const getCategoriesFull = async (): Promise<[]> => {
+  const response = await fetch(`${BASE_URL}/categorias?_embed=videos`);
+  return response.json();
 };
 
-export const getCategories = (): Promise<[]> => {
-  const getData = async () => {
-    const response = await fetch(`${BASE_URL}/categorias`);
-    return response.json();
-  };
-  return getData();
+export const getCategories = async (): Promise<[]> => {
+  const response = await fetch(`${BASE_URL}/categorias`);
+  return response.json();
 };
 
-export const create = (
+export const create = async (
   categoryData: Omit<CategoriasInterface, 'id'>,
 ): Promise<boolean> => {
-  const postData = async () => {
-    const response = await fetch(`${BASE_URL}/categorias`, {
-      method: 'POST',
-      headers: {
-        'Content-type': 'application/json',
-      },
-      body: JSON.stringify(categoryData),
-    });
-    return response.ok;
-  };
-  return postData();
+  const response = await fetch(`${BASE_URL}/categorias`, {
+    method: 'POST',
+    headers: {
+      'Content-type': 'application/json',
+    },
+    body: JSON.stringify(categoryData),
+  });
+  return response.ok;
 };
--- src/repositories/VideosRepository.ts
+++ src/repositories/VideosRepository.ts
@@ -1,17 +1,15 @@
 import BASE_URL from '../config';
 import { VideosInterface } from '../types/video';
 
-export const createVideo = (videoData: VideosInterface): Promise<boolean> => {
-  const postData = async () => {
-    const response = await fetch(`${BASE_URL}/videos`, {
-      method: 'POST',
-      headers: {
-        'Content-type': 'application/json',
-      },
-      body: JSON.stringify(videoData),
-    });
-
-    return response.ok;
-  };
-  return postData();
+export const createVideo = async (
+  videoData: VideosInterface,
+): Promise<boolean> => {
+  const response = await fetch(`${BASE_URL}/videos`, {
+    method: 'POST',
+    headers: {
+      'Content-type': 'application/json',
+    },
+    body: JSON.stringify(videoData),
+  });
+  return response.ok;
 };
